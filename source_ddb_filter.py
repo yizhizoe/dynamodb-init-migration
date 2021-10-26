30a3b8b8ea3b40717b8281f5a6fbdfcd7038f3ca
Zoe Gao <yizhigao@amazon.com>
2021-10-26 17:40:10 +0800

    add param for s3

--- source_ddb_filter.py
+++ source_ddb_filter.py
@@ -6,8 +6,8 @@ from awsglue.context import GlueContext
 from awsglue.job import Job
 
 ## @params: [JOB_NAME]
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
-
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'export_s3_path'])
+s3_path = args['export_s3_path']
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
@@ -29,5 +29,5 @@ applymapping1 = ApplyMapping.apply(frame = filtered_source, mappings = [("street
 ## @args: [connection_type = "s3", connection_options = {"path": "s3://zoe-ssm-output", "compression": "gzip"}, format = "json", transformation_ctx = "datasink2"]
 ## @return: datasink2
 ## @inputs: [frame = applymapping1]
-datasink2 = glueContext.write_dynamic_frame.from_options(frame = applymapping1, connection_type = "s3", connection_options = {"path": "s3://zoe-ssm-output", "compression": "gzip"}, format = "json", transformation_ctx = "datasink2")
+datasink2 = glueContext.write_dynamic_frame.from_options(frame = applymapping1, connection_type = "s3", connection_options = {"path": s3_path, "compression": "gzip"}, format = "json", transformation_ctx = "datasink2")
 job.commit()
